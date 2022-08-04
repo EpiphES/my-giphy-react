7ece56f2e97231d3a5a2976ed2c3bdfbdae5941a
Ekaterina Epiphanova <epiph@yandex.ru>
2022-08-04 12:03:37 +0500

    feat: uninstall masonry

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,8 +1,5 @@
 import "../styles/Gallery.css"
-import {useEffect, useRef} from "react"
 import Gif from "./Gif";
-import Masonry from "masonry-layout";
-
 
 function Gallery({gifs}) {
   const gifElements = gifs.map((item) => {
@@ -10,26 +7,9 @@ function Gallery({gifs}) {
       <Gif key={item.id} gif={item} />
     );
   });
-  const gallery = useRef(null)
-
-  function initMasonry() {
-    console.log("init");
-    return new Masonry(gallery.current, {
-      itemSelector: ".gif",
-      columnWidth: 200,
-      horizontalOrder: true,
-      gutter: 20,
-      fitWidth: true,
-    });    
-         
-  }
-
-  useEffect (() => {
-    initMasonry()
-  })
-
+           
   return (
-    <div className="gallery" ref={gallery} onLoad={initMasonry}>
+    <div className="gallery">
       {gifElements}
     </div>
   );
