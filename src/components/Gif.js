3337b20936b831620e154dd5d2f834218d6766a0
Ekaterina Epiphanova <epiph@yandex.ru>
2022-08-23 15:18:00 +0500

    feat: add go back button

--- src/components/Gif.js
+++ src/components/Gif.js
@@ -1,10 +1,14 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useParams, useHistory } from "react-router-dom";
 import Image from "react-bootstrap/Image";
-import { useParams } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+
 import api from "../utils/api";
 
 
 function Gif() {
+  const history = useHistory();
   const [gif, setGif] = useState({})
   const { id } = useParams();
   function loadGifById(id) {
@@ -13,16 +17,24 @@ function Gif() {
       .then((res) => setGif(res.data))
       .catch((err) => console.log(err));
   }
-  loadGifById(id);
+  useEffect(() => {
+    loadGifById(id)
+  }, [id]);
 
-  return (    
-    <div className="bg-warning rounded">
-      <Image
-        rounded
-        src={gif.images?.original.url}
-        alt={gif.title}
-      />
-    </div>
+
+  return (
+    <Container className="d-flex flex-column justify-content-center pt-3">
+      <div
+        className="mx-auto bg-warning rounded"
+        style={{
+          width: gif.images?.original.width
+        }}>
+        <Image rounded src={gif.images?.original.url} alt={gif.title} />
+      </div>
+      <Button className="mt-3 mx-auto" onClick={() => history.goBack()}>
+        Go back
+      </Button>
+    </Container>
   );
 }
 export default Gif;
