5c595944e5be55e50cd99f81cc4317cd1ed6235b
Ekaterina Epiphanova <epiph@yandex.ru>
2022-08-23 18:01:27 +0500

    feat: add spinner to gif page

--- src/components/Gif.js
+++ src/components/Gif.js
@@ -1,8 +1,9 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { useParams, useHistory } from "react-router-dom";
 import Image from "react-bootstrap/Image";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
+import Spinner from "react-bootstrap/Spinner";
 
 import api from "../utils/api";
 
@@ -11,26 +12,48 @@ function Gif() {
   const history = useHistory();
   const [gif, setGif] = useState({})
   const { id } = useParams();
-  function loadGifById(id) {
-    api
-      .getGifById(id)
-      .then((res) => setGif(res.data))
-      .catch((err) => console.log(err));
-  }
+  const [isLoading, setIsLoading] = useState(false);
+
+  const loadGifById = useCallback(
+    (id) => {
+      setIsLoading(true);
+      api
+        .getGifById(id)
+        .then((res) => setGif(res.data))
+        .catch((err) => console.log(err))
+        .finally(() => setIsLoading(false));
+    },
+    [setIsLoading]
+  );
+  
   useEffect(() => {
     loadGifById(id)
-  }, [id]);
-
+  }, [id, loadGifById]);
 
   return (
-    <Container className="d-flex flex-column justify-content-center pt-3">
-      <div
-        className="mx-auto bg-warning rounded"
-        style={{
-          width: gif.images?.original.width
-        }}>
-        <Image rounded src={gif.images?.original.url} alt={gif.title} />
-      </div>
+    <Container
+      className="d-flex flex-column align-items-center pt-3"
+      >
+      {isLoading ? (
+        <Spinner animation="border" variant="danger" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+      ) : (
+        <div
+          className="mx-auto rounded bg-warning"
+          style={{
+            width: gif.images?.original.width + "px",
+            maxWidth: "100%",
+            aspectRatio: gif.images?.original.width / gif.images?.original.height,
+          }}>
+          <Image
+            rounded
+            src={gif.images?.original.url}
+            alt={gif.title}
+            style={{ maxWidth: "100%" }}
+          />
+        </div>
+      )}
       <Button className="mt-3 mx-auto" onClick={() => history.goBack()}>
         Go back
       </Button>
